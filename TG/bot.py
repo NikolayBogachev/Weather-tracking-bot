7ce77983af52230d13836e58cfde06580c3d544f
Bogachev Nikolay <bogachev.pro@gmail.com>
2024-10-11 13:05:39 +0300

    Создал бота.

---
+++ TG/bot.py
@@ -0,0 +1,13 @@
+
+from aiogram import Bot, Dispatcher
+from config import config
+from aiogram.client.default import DefaultBotProperties
+from aiogram.client.session.aiohttp import AiohttpSession
+from aiogram.enums import ParseMode
+
+
+# Создание сессии и бота
+session = AiohttpSession()
+bot = Bot(token=config.TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML), session=session)
+
+dp = Dispatcher()
